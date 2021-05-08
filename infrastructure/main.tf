4a7e068d90a61474c0dbde8741132cb2125fc2a8
Kabir Gupta <kabirgupta@live.com>
2021-05-08 17:23:24 -0400

    bugt fixes and removing unecessary files

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -33,15 +33,8 @@ resource "aws_security_group" "webserver_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
